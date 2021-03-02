d3e904947c3910f69c89ff8954ed08b0c8b47fd0
dsrice <v.fuji.0223@gmail.com>
2021-03-03 02:31:37 +0900

    少しレイアウトを整える

--- lib/ui/article_detail/article_detail.dart
+++ lib/ui/article_detail/article_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app/model/articledetailmodel.dart';
 import 'package:flutter_app/service/api/articledetailService.dart';
 import 'package:flutter_app/view_model/common/common_view_model.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html/style.dart';
 
 import '../importer.dart';
 
@@ -26,9 +27,24 @@ class _ArticleDetailViewState extends State<ArticleDetailView> {
     _getArticleDetail();
     return Container(
       padding: const EdgeInsets.all(0.0),
-      child: Html(
-        data: _article.body != null ? _article.body : "<p>test</p>",
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const FaIcon(FontAwesomeIcons.chevronLeft),
+          ),
+          actions: [
+            IconButton(icon: const FaIcon(FontAwesomeIcons.chevronRight), onPressed: null)
+          ],
+          backgroundColor: Colors.white,
+        ),
+        body: Column(
+          children: [
+            Text(_article.title, style:  TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+            Html(data: _article.body != null ? _article.body : "<p>test</p>",)
+          ],
+
         )
+      )
     );
   }
 
