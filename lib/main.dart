176c997d188f5be8860df2ad8f9dcf5a115d7de3
dsrice <v.fuji.0223@gmail.com>
2021-03-07 00:56:44 +0900

    色々見なおしてたら作り直したほうがいいと判断

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/ui/common/common.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 
 void main() {
@@ -7,7 +9,20 @@ void main() {
 }
 
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AppState();
+
+}
+
+class _AppState extends State<App>{
+
+  @override
+  void initState() {
+    super.initState();
+    _setIniteMessage();
+    _initFirebaseMessageing();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +30,77 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CommonArea(),
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Column(
+              children: [
+                FaU
+              ],
+            ),
+          ),
+        ),
+      )
     );
+  }
+
+  Future<void> _setIniteMessage() async {
+    await Firebase.initializeApp();
+    RemoteMessage initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    await messaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+
+  void _initFirebaseMessageing(){
+
+
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+        "high_importance_channel",
+        "High Importance Notifications",
+        "This channel is used for important notifications.",
+        importance: Importance.max,
+    );
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("test");
+      const AndroidNotificationChannel channel = AndroidNotificationChannel(
+        "high_importance_channel",
+        "High Importance Notifications",
+        "This channel is used for important notifications.",
+        importance: Importance.max,
+      );
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+
+      // If `onMessage` is triggered with a notification, construct our own
+      // local notification to show to users using the created channel.
+      if (notification != null && android != null) {
+        print(notification.hashCode);
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                "high_importance_channel",
+                "High Importance Notifications",
+                "This channel is used for important notifications.",
+                icon: "ic_stat_name"// other properties...
+              ),
+            ));
+      }
+    });
   }
 }
