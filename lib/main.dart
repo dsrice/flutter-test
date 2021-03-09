a7df2febfb2dc0be4d2229d38c5d92c0165830c5
dsrice <v.fuji.0223@gmail.com>
2021-03-10 02:13:46 +0900

    プッシュ通知関連が復活

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,8 @@ class _AppState extends State<App>{
   @override
   void initState() {
     super.initState();
-    _setIniteMessage();
-    _initFirebaseMessageing();
   }
-/*
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -44,85 +42,5 @@ class _AppState extends State<App>{
       navigatorKey: NavigationService.instance.navigatorKey,
       onGenerateRoute: Routes.generateRoute,
     );
-  }
-*/
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home:Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("main"),
-        ),
-      ),
-    )
-    );
-  }
-
-  Future<void> _setIniteMessage() async {
-    await Firebase.initializeApp();
-    RemoteMessage initialMessage = await FirebaseMessaging.instance.getInitialMessage();
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-    await messaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-    await messaging.setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true,
-    );
-
-    String token = await messaging.getToken();
-    print(token);
-  }
-
-  void _initFirebaseMessageing(){
-
-
-    const AndroidNotificationChannel channel = AndroidNotificationChannel(
-        "base_channel_id",
-        "base_channel_name",
-        "base_channel_descriotion",
-        importance: Importance.max,
-    );
-
-    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-    flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
-        ?.createNotificationChannel(channel);
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print("test");
-      RemoteNotification notification = message.notification;
-      AndroidNotification android = message.notification?.android;
-
-      // If `onMessage` is triggered with a notification, construct our own
-      // local notification to show to users using the created channel.
-      if (notification != null && android != null) {
-        print(notification.hashCode);
-        flutterLocalNotificationsPlugin.show(
-            notification.hashCode,
-            notification.title,
-            notification.body,
-            NotificationDetails(
-              android: AndroidNotificationDetails(
-                  "base_channel_id",
-                  "base_channel_name",
-                  "base_channel_descriotion",
-                icon: "ic_stat_name"// other properties...
-              ),
-            ));
-      }
-    });
   }
 }
--- lib/view/root.dart
+++ lib/view/root.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,8 @@ class _RootViewState extends State<RootView>{
   @override
   void initState(){
     super.initState();
+    _setIniteMessage();
+    _initFirebaseMessageing();
   }
   
   @override
@@ -30,5 +33,72 @@ class _RootViewState extends State<RootView>{
       color: Colors.white
     );
   }
+
+  Future<void> _setIniteMessage() async {
+    await Firebase.initializeApp();
+    RemoteMessage initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    await messaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    String token = await messaging.getToken();
+    print(token);
+  }
+
+  void _initFirebaseMessageing(){
+
+
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      "base_channel_id",
+      "base_channel_name",
+      "base_channel_descriotion",
+      importance: Importance.max,
+    );
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("test");
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+
+      // If `onMessage` is triggered with a notification, construct our own
+      // local notification to show to users using the created channel.
+      if (notification != null && android != null) {
+        print(notification.hashCode);
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                  "base_channel_id",
+                  "base_channel_name",
+                  "base_channel_descriotion",
+                  icon: "ic_stat_name"// other properties...
+              ),
+            ));
+      }
+    });
+  }
+
 
 }
